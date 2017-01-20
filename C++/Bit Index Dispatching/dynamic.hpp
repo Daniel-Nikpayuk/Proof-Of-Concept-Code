5ec354473685185ab5e8348dcf853ac9acb63244
Daniel-Nikpayuk <djharris@ualberta.ca>
2017-01-20 01:14:47 -0700

    latest commit

---
+++ C++/Bit Index Dispatching/dynamic.hpp
@@ -0,0 +1,162 @@
+/************************************************************************************************************************
+**
+** Copyright 2016, 2017 Daniel Nikpayuk
+**
+** This file is part of POCC.
+**
+** nik_debug is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+**
+** nik_debug is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+** of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License along with nik_debug. If not, see
+** <http://www.gnu.org/licenses/>.
+**
+************************************************************************************************************************/
+
+typedef unsigned long size_type;
+
+template<bool cond, typename if_true, typename if_false>
+struct if_then_else
+{
+	using type = if_false;
+};
+
+template<typename if_true, typename if_false>
+struct if_then_else<true, if_true, if_false>
+{
+	using type = if_true;
+};
+
+struct Assoc
+{
+	enum : size_type
+	{
+		omit_count	= 1<<0,
+		apply_count	= 1<<1,
+
+		omit_fun	= 1<<2,
+		apply_fun	= 1<<3
+	};
+};
+
+template<size_type i, typename Filler = void>
+struct Dispatch { };
+
+template<typename Filler>
+struct Dispatch<Assoc::apply_count, Filler> { int count; };
+
+template<size_type index>
+struct Adverb
+{
+	template<size_type x, size_type X>
+	struct is_member
+	{
+		static constexpr bool value = (x & X == x);
+	};
+
+	using Verb = typename if_then_else
+	<
+		is_member<Assoc::apply_count, index>::value,
+		Dispatch<Assoc::apply_count>,
+		Dispatch<Assoc::omit_count>
+
+	>::type;
+
+	Verb verb;
+};
+
+//
+
+template<typename value_type>
+value_type square(const Dispatch<Assoc::omit_count> & a, value_type x)
+{
+	return x*x;
+}
+
+template<typename value_type>
+value_type square(Dispatch<Assoc::apply_count> & a, value_type x)
+{
+	++a.count;
+
+	return x*x;
+}
+
+//
+
+
+/*
+template<size_type index>
+struct Cube
+{
+	struct omit
+	{
+		template<typename value_type>
+		static value_type cube(const Adverb<index> & a, value_type x)
+		{
+			return x*square(a.verb, x);
+		}
+	};
+
+	struct apply
+	{
+		template<typename value_type>
+		static value_type cube(Adverb<index> & a, value_type x)
+		{
+			++a.verb.count;
+
+			return x*square(a.verb, x);
+		}
+	};
+
+	using call = typename if_then_else
+	<
+		Adverb<index>::template is_member<Assoc::apply_count, index>::value,
+		apply,
+		omit
+
+	>::type;
+};
+*/
+
+template<size_type index>
+struct omit
+{
+	template<typename value_type>
+	static value_type cube(const Adverb<index> & a, value_type x)
+	{
+		return x*square(a.verb, x);
+	}
+};
+
+template<size_type index>
+struct apply
+{
+	template<typename value_type>
+	static value_type cube(Adverb<index> & a, value_type x)
+	{
+		++a.verb.count;
+
+		return x*square(a.verb, x);
+	}
+};
+
+
+template<size_type index>
+using Cube = typename if_then_else
+<
+	Adverb<index>::template is_member<Assoc::apply_count, index>::value,
+	apply<index>,
+	omit<index>
+
+>::type;
+
+
+template<size_type index, typename value_type>
+value_type cube(Adverb<index> & a, value_type x)
+{
+	return Cube<index>::cube(a, x);
+}
+
+
--- C++/Bit Index Dispatching/main.cpp
+++ C++/Bit Index Dispatching/main.cpp
@@ -4,28 +4,23 @@
 **
 ** This file is part of POCC.
 **
-** nik_debug is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+** POCC is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
 ** as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 **
-** nik_debug is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+** POCC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 ** of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 **
-** You should have received a copy of the GNU General Public License along with nik_debug. If not, see
+** You should have received a copy of the GNU General Public License along with POCC. If not, see
 ** <http://www.gnu.org/licenses/>.
 **
 ************************************************************************************************************************/
 
-#include<iostream>
-
-//#include"base.hpp"
 #include"dynamic.hpp"
-//#include"static.hpp"
-//#include"template.hpp"
 
 int main(int argc, char *argv[])
 {
 	#define DECLARE(p0, p1, p2, p3)											\
-		Adverb<p0, p1, p2, p3> adv##p0##p1##p2##p3
+		Adverb<p0 | p1 | p2 | p3> adv##p0##p1##p2##p3
 
 	#define CUBE(p0, p1, p2, p3)											\
 		cube(adv##p0##p1##p2##p3, 3)
@@ -35,31 +30,46 @@ int main(int argc, char *argv[])
 		CUBE(p0,p1,p2,p3);
 
 	#define CALL3(p0, p1, p2)											\
-		CALL4(p0,p1,p2,0);											\
 		CALL4(p0,p1,p2,1);											\
 		CALL4(p0,p1,p2,2);											\
-		CALL4(p0,p1,p2,3);
+		CALL4(p0,p1,p2,4);											\
+		CALL4(p0,p1,p2,8);
 
 	#define CALL2(p0, p1)												\
-		CALL3(p0,p1,0);												\
 		CALL3(p0,p1,1);												\
 		CALL3(p0,p1,2);												\
-		CALL3(p0,p1,3);
+		CALL3(p0,p1,4);												\
+		CALL3(p0,p1,8);
 
 	#define CALL1(p0)												\
-		CALL2(p0,0);												\
 		CALL2(p0,1);												\
 		CALL2(p0,2);												\
-		CALL2(p0,3);
+		CALL2(p0,4);												\
+		CALL2(p0,8);
 
 	#define CALL()													\
-		CALL1(0);												\
 		CALL1(1);												\
 		CALL1(2);												\
-		CALL1(3);
+		CALL1(4);												\
+		CALL1(8);
 
 	CALL()
 
+//	Adverb<Assoc::omit_fun | Assoc::apply_count> adv;
+
+//	cube(adv, 3);
+
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
 
